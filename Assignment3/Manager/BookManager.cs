6447d2ffb22650292cd78ea1c96fe489962c9c19
Ziaullah Hassan <Ziaullah Hassan@DESKTOP-2U19JOA>
2021-10-11 12:27:23 -0700

    Add project files.

---
+++ Assignment3/Manager/BookManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment1;
+
+namespace Assignment3.Manager
+{
+    class BookManager
+    {
+        private static List<Book> _Books = new List<Book>() {
+            new Book ("c# programming","Tom",200,"978-3-16-148410-0"),
+            new Book ("Java","Zia",300,"977-3-16-158410-0"),
+            new Book ("c++","khan",400,"976-3-16-168410-0")
+            };
+        public List<Book> GetAll()
+        {
+            return new List<Book>(_Books);
+
+
+        }
+        public Book Get(String ISBN13)
+        {
+            return _Books.Find(book => book.ISBN13 == ISBN13);
+        }
+        public Book Save(Book book)
+        {
+            var _book = _Books.Find(boook => boook.ISBN13 == book.ISBN13);
+            if (_book == null)
+            {
+
+                _Books.Add(book);
+
+                return book;
+            }
+            return null;
+        }
+    }
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,81 @@
+﻿using Assignment1;
+using Assignment3.Manager;
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server");
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Loopback, 4646);
+           
+            //we cant have serval listner
+            listener.Start();// starting the server
+
+            //we need to  clint after starting
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Task.Run(() => { HandleClient(socket); });
+
+                
+
+
+
+            }
+            
+
+
+        }
+        public static void HandleClient(TcpClient socket)
+        {
+            BookManager _manager = new BookManager();
+            NetworkStream stream = socket.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+
+            while (true)
+            {
+                String firstLine = reader.ReadLine();
+                Console.WriteLine(firstLine);
+                String SecondLine = reader.ReadLine();
+                Console.WriteLine(SecondLine);
+                
+
+                switch (firstLine)
+                {
+                    case "Get":
+                        var book = _manager.Get(SecondLine);
+                        writer.WriteLine( JsonSerializer.Serialize(book));
+                        break;
+
+                    case "GetAll":
+                        var _books = _manager.GetAll();
+                        writer.WriteLine(JsonSerializer.Serialize(_books));
+                        break;
+
+                    case "Save":
+                        var boook = _manager.Save(JsonSerializer.Deserialize<Book>(SecondLine));
+                        if(boook != null)
+                        {
+                            writer.WriteLine("book was saved");
+                        }
+                        break;
+                        
+                }
+                writer.Flush();
+            }
+
+
+
+        }
+    }
+    }
+
